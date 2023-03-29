44d06708b76ad2c719a2e0cd310955a7da396fe1
lhshilin <74134634+lhshilin@users.noreply.github.com>
2023-03-29 22:38:34 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 $(function () {
-    $('.dialogBox').css('height', ($(window).height()) * 0.8)
-    $('.dialogBox .left:first-child .time').html(nowTime())
+    var key = true,
+        apikey = localStorage.getItem('apikey'),
+        chatRecords = JSON.parse(localStorage.getItem('chatGPTChatRecords'));
+    $('.dialogBox').css('height', ($(window).height()) * 0.8);
+    $('.dialogBox .left:first-child .time').html(nowTime());
+    $('.dialogBox .leftText:first-child').css('height', $('.dialogBox .leftText:first-child').prop('scrollHeight'))
+    if(chatRecords && chatRecords !== []) {
+        $.each(chatRecords, function (i, e) {
+            if(e.spokesman == 'left') {
+                $('.dialogBox').append('<p class="right"><span class="time">' + e.time + '</span><img src="./images/oneSelf.jpg" alt="logo"></p><p class="rightContent"><textarea class="rightText" disabled>' + e.text +'</textarea></p>')
+                $('.dialogBox .rightText:last').css('height', $('.dialogBox .rightText:last').prop('scrollHeight'))
+            } else {
+                $('.dialogBox').append('<p class="left"><img src="./images/chatGPT.png" alt="chatGPT-log"><span class="time">' + e.time + '</span></p><p class="leftContent"><textarea class="leftText" disabled>' + e.text + '</textarea></p>')
+                $('.dialogBox .leftText:last').css('height', $('.dialogBox .leftText:last').prop('scrollHeight'))
+            }
+        })
+        $('.dialogBox').scrollTop($('.dialogBox').prop('scrollHeight'))
+    } else {
+        localStorage.setItem('chatGPTChatRecords', "[]");
+        chatRecords = [];
+    }
     function nowTime() {
         var time = new Date();
         function zeroPadding(num) {
@@ -9,35 +28,70 @@ $(function () {
             }
             return num
         }
-        time = zeroPadding(time.getHours()) + ':' + zeroPadding(time.getMinutes()) + ':' + zeroPadding(time.getSeconds());
+        time = time.getFullYear() + '年' + (time.getMonth() + 1) + '月' + time.getDate() + '日 ' + zeroPadding(time.getHours()) + ':' + zeroPadding(time.getMinutes()) + ':' + zeroPadding(time.getSeconds());
         return time
     }
-    if(!localStorage.getItem('apikey')) {
+    if(!apikey) {
         $('.apikey').show()
         $('.cover').show()
         $('body').css('overflow', 'hidden')
+        $('.apikeyInput').focus()
+    } else {
+        $('.text').focus()
+    }
+    function getApiKey() {
+        var apikeyInput = $('.apikeyInput').val();
+        apikey = apikeyInput;
+        localStorage.setItem('apikey', apikeyInput)
+        $('.apikey').hide(300)
+        $('.cover').hide()
+        $('body').css('overflow', 'visible')
+        $('.text').focus()
     }
     $('.apikeyBtn').on('click', function () {
-        if($('.apikeyInput').val()) {
-            localStorage.setItem('apikey', $('.apikeyInput').val())
-            $('.apikey').hide(300)
-            $('.cover').hide()
-            $('body').css('overflow', 'visible')
+        if($('.apikeyInput').val() !== '') getApiKey()
+    })
+    $('.apikey .apikeyInput').on('keydown', function (e) {
+        if(e.keyCode == 13) {
+            if($(this).val() !== '') getApiKey()
         }
     })
+    function returnData(data) {
+        var time = nowTime();
+        chatRecords.push({
+            spokesman : 'right',
+            time : time,
+            text : data
+        });
+        localStorage.setItem('chatGPTChatRecords', JSON.stringify(chatRecords));
+        $('.dialogBox .leftText:last').html($.trim(data)).css('height', $('.dialogBox .leftContent:last-child .leftText').prop('scrollHeight'))
+        $('.dialogBox .left:nth-last-child(2) .time').html(time)
+        $('.dialogBox').scrollTop($('.dialogBox').prop('scrollHeight'))
+        key = true;
+        $('.send').on('click', getData)
+    }
     // 得到回复信息
     function getData() {
+        key = false;
+        $('.send').off('click')
         var text = $.trim($('.text').val());
         if(text === '') return;
-        $('.dialogBox').append('<p class="right"><span class="time">' + nowTime() + '</span><img src="./images/oneSelf.jpg" alt="logo"></p><p class="rightContent"><textarea class="rightText" disabled>' + text +'</textarea></p><p class="left"><img src="./images/chatGPT.png" alt="chatGPT-log"><span class="time"></span></p><p class="leftContent"><textarea class="leftText" disabled>chatGPT正在思考中...</textarea></p>')
+        var time = nowTime();
+        chatRecords.push({
+            spokesman : 'left',
+            time : time,
+            text : text
+        });
+        localStorage.setItem('chatGPTChatRecords', JSON.stringify(chatRecords));
+        $('.dialogBox').append('<p class="right"><span class="time">' + time + '</span><img src="./images/oneSelf.jpg" alt="logo"></p><p class="rightContent"><textarea class="rightText" disabled>' + text +'</textarea></p><p class="left"><img src="./images/chatGPT.png" alt="chatGPT-log"><span class="time"></span></p><p class="leftContent"><textarea class="leftText" disabled>chatGPT正在思考中...</textarea></p>')
         $('.text').val('')
         $('.dialogBox .rightText:last').css('height', $('.dialogBox .rightText:last').prop('scrollHeight'))
         $('.dialogBox').scrollTop($('.dialogBox').prop('scrollHeight'))
-        var Authorization = 'Bearer ' + localStorage.getItem('apikey'),
+        var Authorization = 'Bearer ' + apikey,
             data = JSON.stringify({
                 model : 'text-davinci-003',
                 user : '1',
-                max_tokens : 1000,
+                max_tokens : 2000,
                 temperature : 0.5,
                 frequency_penalty : 0,
                 presence_penalty : 0,
@@ -53,15 +107,15 @@ $(function () {
             },
             data : data,
             success : function (res) {
-                $('.dialogBox .leftText:last').html($.trim(res.choices[0].text)).css('height', $('.dialogBox .leftContent:last-child .leftText').prop('scrollHeight'))
-                $('.dialogBox .left:nth-last-child(2) .time').html(nowTime())
-                $('.dialogBox').scrollTop($('.dialogBox').prop('scrollHeight'))
+                returnData(res.choices[0].text)
             },
             error : function (err) {
-                err = JSON.parse(err.responseText)
-                $('.dialogBox .leftText:last').html($.trim(err.error.message)).css('height', $('.dialogBox .leftContent:last-child .leftText').prop('scrollHeight'))
-                $('.dialogBox .left:nth-last-child(2) .time').html(nowTime())
-                $('.dialogBox').scrollTop($('.dialogBox').prop('scrollHeight'))
+                if(err.responseText) {
+                    err = JSON.parse(err.responseText)
+                    returnData(err.error.message)
+                } else {
+                    returnData('对不起，chatGPT不能收到你的消息')
+                }
             }
         })
     }
@@ -76,6 +130,7 @@ $(function () {
                 $('.apikey').fadeIn(300)
                 $('.cover').show()
                 $('body').css('overflow', 'hidden')
+                $('.apikeyInput').focus()
             }
         }, 1000)
         return timer
@@ -98,11 +153,13 @@ $(function () {
             }) 
         }
     })
-    $('.send').on('click', getData)
-    $('.apikeyInput').on('keydown', function (e) {
-        if(e.keyCode == 13) $('.apikeyBtn').click();
+    $('.header span').on('dblclick', function () {
+        chatRecords = [];
+        localStorage.setItem('chatGPTChatRecords', "[]");
+        $('.dialogBox').html('')
     })
+    $('.send').on('click', getData)
     $('.text').on('keydown', function (e) {
-        if(e.keyCode == 13) getData();
+        if(e.keyCode == 13 && key) getData();
     })
 })
